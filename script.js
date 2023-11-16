2fcd1982e85982aa7b44b16c12e853e3d73c81b5
Yash Kumar <85291913+yashsaini7060@users.noreply.github.com>
2023-11-16 20:48:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 var dateDiffInDays = function (date1, date2) {
   //   write your code here
+	
+	var date1_ms = Date.parse(date1);
+	var date2_ms = Date.parse(date2);
+	var difference= date2_ms-date1_ms;
+	var days = difference/1000/60/60/24;
+	return days;
 };
 
 // Do not change the code below.
